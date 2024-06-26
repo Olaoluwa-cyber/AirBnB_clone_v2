a8cdc74969376cf61f72e4a9a962b922fb573614
Olaoluwa-cyber <oladipovictor007@gmail.com>
2024-06-26 15:56:08 -0700

    r

--- models/user.py
+++ models/user.py
@@ -16,4 +16,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref="user", cascade="all, delete")
     reviews = relationship("Review", backref="user", cascade="all, delete")
-
